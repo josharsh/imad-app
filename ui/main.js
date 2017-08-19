b78331e0c79bc42ef2f27c9df8034d7f1d0c7ca0
Harsh Joshi <harsh.joshi.pth@gmail.com>
2017-08-19 15:38:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,15 @@ var element = document.getElementById('maintxt');
 element.innerHTML="New Value";
 
 var picture= document.getElementById('PIC');
-    PIC.onclick= function(){
+    PIC.onclick= function()
+    {var marginLeft=0;
+        function moveRight()
+    {
+        marginLeft=marginLeft+10;
+      PIC.style.marginLeft = marginLeft+'px';
+        
+    }
+        var interval= setInterval(moveRight, 100);
     PIC.style.marginLeft='100px';
     
     
