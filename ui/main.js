0d4d87a6e02b9f005f19f327b0d7e2d09f7fa276
Harsh Joshi <harsh.joshi.pth@gmail.com>
2017-08-19 16:43:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,9 @@
-var btn= document.getElementById('count');
+var btn= document.getElementById('counter');
 var ccounterclick=0;
 btn.onClick(function(){
   
   ccounterclick=ccounterclick+1;
-  var spn=document.getElementById('count');
+  var spn=document.getElementById('countclick');
   spn.innerHTML=ccounterclick.toString();
 });
     
