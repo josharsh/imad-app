409d623fb3eba24d05eb01bf96f8262de8d86ec0
Harsh Joshi <harsh.joshi.pth@gmail.com>
2017-08-25 10:57:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,5 +26,20 @@ button.onClick =function () {
 };
     */
     
+
+
+//counter code
+var button=document.getElementById('counter');
+button.onClick = function() {
+    //Make a request to counter endpoint
     
+    
+    //capture the response and store it in a variable
+    
+    
+    //render the variable in correct span
+    counter = counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML= counter.toString();
+};
     
