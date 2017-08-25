574d7aed101908ff12fa7e4963b754ffbc0ad52a
Harsh Joshi <harsh.joshi.pth@gmail.com>
2017-08-25 10:41:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,23 @@
 var button= document.getElementById('counter');
 
 button.onClick =function () {
+    
+    
+    
     var request= new XMLHttpRequest();
+    
     request.onreadystatechange = function(){
         if(request.readyState===XMLHttpRequest.DONE());{
             if (request.status === 200) {
                 var counter = request.responseText;
                 var span= document.getElementById('count');
                 span.innerHTML=counter.toString();
+            }
         }
-    }};
+    };
         
 
-    request.open('GET', 'https://harshjoshipth.imad.hasura-app.io/counter', true);
+    request.open('GET', 'http://harshjoshipth.imad.hasura-app.io/counter', true);
     request.send(null);
     
     
